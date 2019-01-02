b55b4c529a2c14f217d3ebc2a3127b4b93fbea33
SadCyberTeam <45815442+SADCyberTeam@users.noreply.github.com>
2019-01-02 12:30:07 +0700

    Create Spamer.sh

---
+++ Spamer.sh
@@ -0,0 +1,90 @@
+  clear
+
+figlet -f bigmono9 "Spam Sms ::v" | lolcat
+
+echo "\033[38;1m ------------------------------------------------------------------"|lolcat
+
+echo "\033[31;1m "
+
+echo "\033[36;1m" " By Tn. Star :v "
+
+echo "\033[35;1m #Budidayakan Spam Mantan#"
+
+echo "\033[38;1m ------------------------------------------------------------------"|lolcat
+
+date | lolcat
+
+echo ""
+
+     echo "\033[34;1m"" SPAM SMS To Mantan >:'v "
+
+    
+
+     echo "\033[32;1m"" (01)TELKOMSEL "
+
+     echo "\033[33;1m"" (02)PHD"
+
+     echo "\033[32;1m"" (03)TOKOPEDIA" 
+
+     echo "\033[34;1m"" (0)INFO "
+
+     echo "\033[34;1m"" (99)Keluar "
+
+echo "\033[33;1m"" pilih Senjata lu cuk!  :v"
+
+read ezz 
+
+
+if [ $ezz = 02 ] || [ $ezz = 1]
+
+then
+
+clear
+
+toilet -f lean "TELKOMSEL"
+
+php telkomsel.php
+
+sh Spamer.sh
+
+fi
+
+
+if [ $ezz = 03 ] || [ $ezz = 2 ]
+
+then
+
+clear 
+
+toilet -f lean "PHD"
+
+php phd.php
+
+sh Spamer.sh
+
+fi
+
+if [ $ezz = 05 ] || [ $ezz = 3 ]
+
+then
+
+clear
+
+python2 lazada.py
+
+sh Spamer.sh
+
+fi
+
+
+if [ $ezz = 99 ]
+
+then
+
+echo "semoga MantanMu senang gan :v.   "
+
+echo " THX Ferguoso :v "
+
+fi
+
+
