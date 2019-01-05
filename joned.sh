19f8266d7c277ea14924d7d7e47b65372c48329a
SadCyberTeam <45815442+SADCyberTeam@users.noreply.github.com>
2019-01-05 13:12:28 +0700

    Update joned.sh

--- joned.sh
+++ joned.sh
@@ -3,32 +3,15 @@
  figlet Star   |lolcat
 
 echo -e
-"_________________________________________________$
+"_________________________________________________"
 echo -e  "Tools    : ./Tuan Star $white "|lolcat
 echo -e  "Created by  : Star $white   "|lolcat
 echo -e  "From  : S.A.D Cyber Team $cyan " |lol$
-echo -e "_________________________________________$
-
-###################################################
-# CTRL + C
-###################################################
-trap ctrl_c INT
-ctrl_c() {
-clear
-echo -b $red"[#]> (Ctrl + C ) Detected, Trying To $
-echo -b $cyan"[#]> Thanks"
-sleep 1
+echo -e "_________________________________________"
 echo ""
-echo -b  $white"[#]> see you gaes :)..."
-sleep 1
-exit
-}
-
-lagi=1
-while [ $lagi -lt 6 ];
-do
 echo ""
-echo -e $b "1.  Upgrade${enda}";
+echo ""
+echo -e $b "1.  Tools${enda}";
 echo -e "============================" | lolcat
 echo -e $r "2.  Info${endc}";
 echo -e "============================" | lolcat
@@ -50,5 +33,8 @@ if [ $pil = 2 ]
 then 
 clear
 echo " Thanx Mohon Bersabar"
+echo "coming Tools Installer v3"
+echo "By : TuanStar "
+echo ""
 
 fi 
